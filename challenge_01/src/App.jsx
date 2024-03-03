1b047901706bf8eda601261b590672afbc1a1a1c
Rishabh Gupta <r1shabh@Rishabhs-MacBook-Air.local>
2024-03-03 14:06:39 +0530

    fix: Updated profile photo path

--- challenge_01/src/App.jsx
+++ challenge_01/src/App.jsx
@@ -5,13 +5,13 @@ export default function App() {
     <div className="bg-[#141414] h-screen flex items-center justify-center">
       <div className="bg-[#1F1F1F] flex flex-col items-center py-8 px-8 rounded-lg">
         {/* Upper */}
-        <div className="flex flex-col gap-2 items-center mb-6">
+        <div className="flex flex-col items-center gap-2 mb-6">
           <img
             className="rounded-full w-28 h-28"
-            src="/src/assets/images/avatar-jessica.jpeg"
+            src="/assets/images/avatar-jessica.jpeg"
             alt="profile pic"
           />
-          <h1 className="text-white font-semibold text-xl">Jessica Randall</h1>
+          <h1 className="text-xl font-semibold text-white">Jessica Randall</h1>
           <h3 className="text-[#A8C062] mb-2 text-base">
             London, United Kingdom
           </h3>
@@ -20,7 +20,7 @@ export default function App() {
           </p>
         </div>
 
-        <div className="flex gap-3 flex-col">
+        <div className="flex flex-col gap-3">
           <Button title="Github" />
           <Button title="Frontend Mentor" />
           <Button title="LinkedIn" />
